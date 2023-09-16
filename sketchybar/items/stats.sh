9f95c9c85cd0dee3fdb0d69ce8cf03b3aa87c068
Alexander Yususpov <alex@klepa.co>
2023-09-17 00:20:14 +0800

    Updates

--- sketchybar/items/stats.sh
+++ sketchybar/items/stats.sh
@@ -1,7 +1,7 @@
 
 sketchybar --add alias "Stats,Disk_mini" right \
            --set "Stats,Disk_mini" paddng_right=-15 padding_left=-10 label.width=0 \
-                 click_script="open -a /System/Applications/Utilities/Activity\ Monitor.app"
+                 click_script="open -a /System/Applications/Utilities/Disk\ Utility.app"
 
 sketchybar --add alias "Stats,RAM_mini" right \
            --set "Stats,RAM_mini" paddng_right=-15 padding_left=-10 label.width=0 \
